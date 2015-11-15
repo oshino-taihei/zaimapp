b412e7e6f4c8d5cfa4bddf70e0dccdaca7322348
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-11-15 17:44:12 +0900

    D3.jsでなくflotr2を使ってグラフ描画するように修正(ただしグラフはまだダミー)

--- app/controllers/data_view_controller.rb
+++ app/controllers/data_view_controller.rb
@@ -1,2 +1,8 @@
 class DataViewController < ApplicationController
+  def month
+    money = Money.group('substr(date, 1, 7)').group(:zaim_category_id).group(:zaim_genre_id).having('substr(date, 1, 7) = "2015-09"').order(amount: :desc).sum(:amount)
+    @data = money.map do |k,v|
+      {month: k[0], category: k[1], genre: k[2], amount: v}
+    end
+  end
 end
